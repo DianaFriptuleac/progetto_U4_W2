3c5a1fc3a47f0a83b3d7037f01bbce32ab4efc6c
Diana <dianadorojuc3@gmail.com>
2024-10-04 13:49:09 +0200

    ex 2 ricerca per id

--- src/main/java/Diana_Friptuleac/Collezione.java
+++ src/main/java/Diana_Friptuleac/Collezione.java
@@ -1,28 +1,39 @@
 package Diana_Friptuleac;
 
 import Diana_Friptuleac.Classi.AllGiochi;
-import Diana_Friptuleac.Classi.Videogioco;
 import exceptions.DuplicateException;
 
 import java.util.List;
-
+import java.util.Optional;
 
 public class Collezione {
 
-    private List<Videogioco> giochi;
+    private List<AllGiochi> giochi;
 
-    public Collezione(List<Videogioco> giochi) {
+
+    public Collezione(List<AllGiochi> giochi) {
         this.giochi = giochi;
     }
 
-    //Aggiunta di un elemento
+    // Aggiungo un elemento
     public void aggGioco(AllGiochi giocoDaAgg) {
         if (giochi.stream().anyMatch(gioco -> gioco.getId() == giocoDaAgg.getId())) {
             throw new DuplicateException("Non puoi inserire un gioco con lo stesso Id!");
         } else {
-            giochi.add((Videogioco) giocoDaAgg);
-            System.out.println("Il gioco" + giocoDaAgg.getTitolo() + ", ID: " + giocoDaAgg.getId() + " e stato aggiunto!");
+            giochi.add(giocoDaAgg); // Aggiunge l'elemento alla collezione
+            System.out.println("Il gioco " + giocoDaAgg.getTitolo() + ", ID: " + giocoDaAgg.getId() + " è stato aggiunto!");
         }
     }
 
+    // Cerco per ID
+    public void trovaGioco(int id) {
+        Optional<AllGiochi> giocoTrovato = giochi.stream()
+                .filter(gioco -> gioco.getId() == id)
+                .findFirst();
+        if (giocoTrovato.isPresent()) {
+            System.out.println("Il gioco che stavi cercando è: " + giocoTrovato.get().getTitolo() + " (ID: " + giocoTrovato.get().getId() + ")");
+        } else {
+            System.out.println("Il gioco con l'id " + id + " non è stato trovato.");
+        }
+    }
 }
